f4963ecf79f05179683069117f942e598c993a3a
YIcheng Tsai <yichengtsai@dhcp-206-12-137-211.ubcsecure.wireless.ubc.ca>
2020-02-18 15:38:22 -0800

    add files

---
+++ minutizer-react/src/Tabs/transcribeTab.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class TranscribeTab extends Component {
+    render() {
+        return(
+            <h2>transcribe audio file</h2>
+        );
+    }
+}
+export default TranscribeTab;
+
+
