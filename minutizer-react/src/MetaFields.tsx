7af5315a3b5f3881ddf98c8de102337e73d6ffb3
Joshua Rayo <j.rayo123@gmail.com>
2020-02-20 02:57:31 -0800

    debug uploadToCloud() - issue with constructor

--- minutizer-react/src/MetaFields.tsx
+++ minutizer-react/src/MetaFields.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import AttendeesComponent from "./AttendeesComponent";
 import './MetaFields.css';
 
-export default class NameForm extends React.Component<{}, {organizer: any, startTime: any, endTime: any}> {
+export default class MetaFields extends React.Component<{}, {organizer: any, startTime: any, endTime: any}> {
     private fileInput = React.createRef<HTMLInputElement>();
     constructor(props: any) {
         super(props);
@@ -35,6 +35,7 @@ export default class NameForm extends React.Component<{}, {organizer: any, start
             if (extension === "wav" || extension === "mp3") {
                 // @ts-ignore: Object is possibly 'null'.
                 alert(`Selected file - ${this.fileInput.current.files[0].name}`);
+                this.uploadToCloud();
                 return true;
             } else {
                 alert('Invalid file. Only .mp3 or .wav files accepted');
@@ -43,6 +44,50 @@ export default class NameForm extends React.Component<{}, {organizer: any, start
         }catch (e) {
             return false;
         }    
+    }
+
+    private uploadToCloud() {
+        // Imports the Google Cloud client library
+        const { Storage } = require("@google-cloud/storage");
+        const path = require("path");
+        const GOOGLE_CLOUD_PROJECT_ID = "hacksbc-268409"; // Replace with your project ID
+        // const GOOGLE_CLOUD_KEYFILE = path.join(__dirname,"../hacksbc-268409-3783f43ce9f1.json"); // Replace with the path to the downloaded private key
+        const GOOGLE_CLOUD_KEYFILE = "Desktop/hacksbc-268409-3783f43ce9f1.json";
+
+        // console.log(GOOGLE_CLOUD_KEYFILE);
+        // console.log(__dirname);
+
+        // Creates a client
+        // SOMETHING'S WRONG WITH THIS CONSTRUCTOR
+        const storage = Storage({
+            projectId: GOOGLE_CLOUD_PROJECT_ID,
+            keyFilename: GOOGLE_CLOUD_KEYFILE,
+        });
+
+        console.log("got here");
+        storage.getBuckets().then( (x:any) => console.log(x));
+
+        const bucketName = "minutizer_recordings";
+        const filename = path.join(__dirname,"./test.txt");
+
+        console.log(filename);
+
+        async function uploadFile() {
+            // Uploads a local file to the bucket
+            await storage.bucket(bucketName).upload(filename, {
+                // Support for HTTP requests made with `Accept-Encoding: gzip`
+                // By setting the option `destination`, you can change the name of the
+                // object you are uploading to a bucket.
+                metadata: {
+                    // Enable long-lived HTTP caching headers
+                    // Use only if the contents of the file will never change
+                    // (If the contents will change, use cacheControl: 'no-cache')
+                    cacheControl: 'public, max-age=31536000',
+                },
+            });
+            console.log(`${filename} uploaded to ${bucketName}.`);
+        }
+        uploadFile().catch(console.error);
     }
 
     render() {
@@ -62,7 +107,7 @@ export default class NameForm extends React.Component<{}, {organizer: any, start
                 </label>
                 <AttendeesComponent></AttendeesComponent>
                 <br/>
-                <input type="file" id="inputFile" ref={this.fileInput} onChange={() => this.handleFileSubmit(this)} />
+                <input type="file" accept = "audio/*" id="inputFile" ref={this.fileInput} onChange={() => this.handleFileSubmit(this)} />
             </form>
         );
     }
